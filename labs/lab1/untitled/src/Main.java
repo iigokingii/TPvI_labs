6c32adbeecdf871c6cdedd52877fbdb0c044ce49
iigokingii <smolik.valera@mail.ru>
2023-04-14 18:09:27 +0300

    testcom

---
+++ labs/lab1/untitled/src/Main.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+import java.net.Socket;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Main {
+    public static int a;
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello world!");
+    }
+}
